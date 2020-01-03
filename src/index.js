3cbc88697cb56ca352bdc809344995df65aa587b
Filipe <flpgst@gmail.com>
2020-01-03 17:58:25 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import 'react-native-gesture-handler'
+import { View } from 'react-native'
+
+import './config/ReactotronConfig'
+
+export default function App() {
+  return <View />
+}
